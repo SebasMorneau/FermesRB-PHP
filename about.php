2a1b05355e7b0496e8aa339087c64b27144439e9
Sebastien <morneau_sebastien@outlook.com>
2019-06-15 11:03:57 -0400

    Changement de la page About - Display cards

--- about.php
+++ about.php
@@ -37,6 +37,17 @@
         <!-- /.container-fluid -->
     </nav>
 
+
+
+
+
+
+
+
+
+
+
+
     <!-- Header -->
     <header>
         <div class="container">
@@ -45,35 +56,60 @@
             </div>
         </div>
     </header>
-		<div class="textePresentation">
-            <div class="texteAbout">
-                <h2>Une entreprise familiale</h2>
-    			<p style="font-size:16px;">Les fermes sont dirigées par Yvan Lépine depuis 2005. Ses fils Antoine et Philippe, se sont joint à leur père en
-                    tant que copropriétaires en août 2016.</p>
-    			<img src="img/famille2.jpg" class="img-responsive" alt="">
-            </div>
 
-            <div class="texteAbout">
-                <p style="font-size:16px;">L'entreprise produit des pommes de terre depuis plus de 30 ans. Elle produit environ de 8 à 10 variétés par année.
-                    La ferme cultive 500 acres de pommes de terre. Ce qui donne une récolte d'environ 12 à 15 millions de livres.</p>
-    			<img src="img/imageTruck.jpg" class="img-responsive" alt="">
-            </div>
 
-            <div class="texteAbout">
-                <p style="font-size:16px;">Elle emploie jusqu'à 20 personnes durant les fortes périodes de récoltes. La compagnie fait aussi l'emballage
-                    et la distribution de leurs produits au Québec, ainsi que dans l'est des États-Unis. Elle offre aussi la possibilité
-                    d'aller chercher ces pommes de terre directement à la ferme.</p>
-                <img src="img/logo2.JPG" class="img-responsive" alt="">
-            </div>
 
-            <div class="texteAbout">
-    			<p style="font-size:16px;">Une famille dévouée, prête à vous offrir ces produits frais et locaux.</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Une entreprise familiale</h5>
+                        <p class="card-text-center">Les fermes sont dirigées par Yvan Lépine depuis 2005. Ses fils Antoine et Philippe, se sont joint à leur père en
+                        tant que copropriétaires en août 2016.</p>
+                    </div>
+                </div>
             </div>
-
-            <div class="texteAbout">
-			    <h2>De la terre, directement à votre assiette.</h2>
-			</div>
-		</div>
+            <div class="col-sm-6">
+                <div class="card-img">
+                    <img src="img/famille2.jpg" class="img-responsive" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+        <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Une famille dévouée, prête à vous offrir ces produits frais et locaux.</h5>
+                        <p class="card-text-center">L'entreprise produit des pommes de terre depuis plus de 30 ans. Elle produit environ de 8 à 10 variétés par année.
+                        La ferme cultive 500 acres de pommes de terre. Ce qui donne une récolte d'environ 12 à 15 millions de livres.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <img src="img/logo2.JPG" class="img-responsive" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">De la terre, directement à votre assiette.</h5>
+                        <p class="card-text-center">Elle emploie jusqu'à 20 personnes durant les fortes périodes de récoltes. La compagnie fait aussi l'emballage
+                        et la distribution de leurs produits au Québec, ainsi que dans l'est des États-Unis. Elle offre aussi la possibilité
+                        d'aller chercher ces pommes de terre directement à la ferme.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <img src="img/imageTruck.jpg" class="img-responsive" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 <?php
