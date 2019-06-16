21d50fd7f95b12e75a7aca095fe78bc7ab9018b9
Sebastien <morneau_sebastien@outlook.com>
2019-06-16 08:37:17 -0400

    Delay des animations

--- partial/footer.php
+++ partial/footer.php
@@ -109,7 +109,7 @@
         /* Check the location of each desired element */
         $('.hideme').each( function(i){
             
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_object = $(this).position().top + $(this).outerHeight() -150;
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             /* If the object is completely visible in the window, fade it it */
@@ -120,6 +120,8 @@
             }
             
         }); 
+
+       
     
     });
     
